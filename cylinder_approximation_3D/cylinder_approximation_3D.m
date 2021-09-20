0c5cdad55f6057962f3fe8b6544f184a2f1d7b8f
chtaimoor <80115835+chtaimoor@users.noreply.github.com>
2021-09-20 16:18:46 +0200

    Update cylinder_approximation_3D.m

--- cylinder_approximation_3D/cylinder_approximation_3D.m
+++ cylinder_approximation_3D/cylinder_approximation_3D.m
@@ -1,11 +1,15 @@
-%clc; clear; close all;
+clc; clear; close all;
 
-% F = [];
-% V = [];
-% N = [];
-% number_of_sections = 0;
-% [mesh_list, y_values] = create_sections(F,V,N,number_of_sections);
-% 
+ F = [];
+ V = [];
+ N = [];
+ number_of_sections = 0;
+ [mesh_list, y_values] = create_sections(F,V,N,number_of_sections);
+
+ % mesh_list = [];
+ %y_values = [0, 50, 150, 190, 300, 400, 550, 600];
+ 
+ figure();
 [polygone_list, y_values] = define_2D_polygones(mesh_list, y_values);
 
 % Task 3: (I use dummy input in the function itself
