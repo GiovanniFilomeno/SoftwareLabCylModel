c73901f72c16d6b6659a46b631b047cce439f3c2
cristiansaenzb <80332251+cristiansaenzb@users.noreply.github.com>
2021-09-23 17:06:01 +0200

    Update create_sections.m
    it works now in the y-axis

--- cylinder_approximation_3D/create_sections.m
+++ cylinder_approximation_3D/create_sections.m
@@ -1,53 +1,49 @@
 function [mesh_list, y_values] = create_sections(F,V,N,number_of_sections)
 
-% mesh_list = [[F,V,N],[F,V,N],...] for each section
-% y_values has length number_of_sections + 1 (each section bottom and top)
-
 % Read the original geometry
-%[F,V,N] = stlread("Example.stl");
-[F,V,N] = stlread("Pyramid Shape.stl");
+[F,V,N] = stlread("simple_cube.stl");
 
-XminGEO=min(V(:,1));
-XmaxGEO=max(V(:,1));
-%XcutGEO=linspace(XminGEO,XmaxGEO,number_of_sections);
+YminGEO=min(V(:,2));
+YmaxGEO=max(V(:,2));
+number_of_sections=7;
+delta=(YmaxGEO-YminGEO)/number_of_sections;
 
-%XcutGEO=[0 50 150 190 300 400 550 600];
-XcutGEO=[-20 -15 -5 5 15 20];
-
-y_values=XcutGEO;
-mesh_list = cell((length(XcutGEO)-1),3);
+YcutGEO=[YminGEO (YminGEO+delta*1) (YminGEO+delta*2) (YminGEO+delta*3) (YminGEO+delta*4) (YminGEO+delta*5) (YminGEO+delta*6)];
+y_values=YcutGEO;
+mesh_list = cell((length(YcutGEO)),3);
 
 
-for i=1:(length(XcutGEO)-2)
+for i=1:(length(YcutGEO)-1)
     
-%1 CUT
-[VGR,NGR,FGR,VRD,NRD,FRD]=cut_the_geometry(V,N,F,XcutGEO(i+1));
+    [FGR,VGR,NGR,FRD,VRD,NRD]=cut_the_geometryV2(F,V,N,YcutGEO(i+1));
 
-mesh_list{i,1}=FGR;
-mesh_list{i,2}=VGR;
-mesh_list{i,3}=NGR;
+    mesh_list{i,1}=FGR;
+    mesh_list{i,2}=VGR;
+    mesh_list{i,3}=NGR;
 
-V=VRD;
-N=NRD;
-F=FRD;
+    F=FRD;
+    V=VRD;
+    N=NRD;
 
-if i==(length(XcutGEO)-2)
-    mesh_list{i+1,1}=FRD;
-    mesh_list{i+1,2}=VRD;
-    mesh_list{i+1,3}=NRD;
-end
+    if i==(length(YcutGEO)-1)
+        mesh_list{i+1,1}=FRD;
+        mesh_list{i+1,2}=VRD;
+        mesh_list{i+1,3}=NRD;
+    end
 
 end
 
 color=['g';'m';'b';'g';'m';'b';'g'];
 
-for j=1:(length(XcutGEO)-1)
+for j=1:(length(YcutGEO))
     
     print_STL( mesh_list{j,2}, mesh_list{j,1},color(j));
     hold on
     
 end
 
-
 end
+
+ 
+
 
